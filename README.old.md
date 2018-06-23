b3e251361c1c3985fb86a50e12516e0324df13fd
wssjdi@gmail.com <wssjdi@gmail.com>
2018-06-23 12:08:51 +0800

    React生命周期

---
+++ README.old.md
@@ -0,0 +1,45 @@
+# admin-hades
+Admin On React &amp; Antd
+
+一、项目初始化：
+npm install -g create-react-app
+create-react-app my-app-name
+
+cd my-app-name
+npm start
+
+二、React生命周期包含哪些：
+getDefaultProps
+getInitialState
+componentWillMount
+render
+componentDidMount
+componentWillReceiveProps
+shouldComponentUpdate
+componentWillUpdate
+componentDidUpdate
+componentWillUnmount
+
+三、安装基础插件：
+
+安装React-Router：
+yarn add react-router-dom axios less-loader
+
+安装Axios：
+
+安装AntD：
+
+暴露Webpack配置文件：
+ yarn eject
+
+安装less-loader:
+  AntD是基于Less开发的，但是React脚手架并没有安装Less,所以需要暴露Webpack配置之后安装less-loader;
+
+修改less-loader：
+
+
+
+
+四、Less文件加载配置：
+
+
---
+++ src/pages/demo/Child.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+
+export default class Child extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      count:0
+    };
+  }
+
+  componentWillMount(){
+    console.log("will mount");
+  }
+
+  componentDidMount(){
+    console.log("did mount");
+  }
+
+  componentWillReceiveProps(newProps){
+    console.log("will receive props"+newProps.name);
+  }
+
+  shouldComponentUpdate(){
+    console.log("should update");
+    return true;
+  }
+  
+  componentWillUpdate(){
+    console.log("will update");
+  }
+  
+  componentDidUpdate(){
+    console.log("did update");
+  }
+
+  componentWillUnmount(){
+    console.log("will unmount");
+  }
+
+  render(){
+    return <div>
+      <p>这里是Child组件</p>     
+      <p>{this.props.name}</p>     
+    </div>
+  }
+
+
+
+
+
+
+
+
+}  
